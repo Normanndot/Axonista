343d7ab655b3d1253bff349f87a3fb0d24cb7391
normanndot <normanndot@gmail.com>
2022-08-12 23:39:10 +0530

    Separated the network layer

--- Axonista/Network/MovieAPIRequest.swift
+++ Axonista/Network/MovieAPIRequest.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import NetworkLibrary
 
 enum MovieAPIRequest {
     case movies
--- Axonista/Network/MovieFetcher.swift
+++ Axonista/Network/MovieFetcher.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import NetworkLibrary
 
 protocol MovieFetchable {
     func movies() async -> Result<Movies, RequestError>
